9f8c5f6b794ffdfb15919efe2c5937cf72eff644
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-03-21 14:22:26 -0400

    adds button to show specific review but not edit yet

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -1,11 +1,15 @@
 <script>
 import axios from "axios";
+// import SavedModal from "../components/SavedModal.vue";
 export default {
+  // components: { SavedModal },
   data: function () {
     return {
       user: {},
       newReviewParams: {},
+      editReviewParams: {},
       errors: [],
+      // showModal: false,
     };
   },
   created: function () {
@@ -29,6 +33,25 @@ export default {
           console.log(this.errors);
         });
     },
+    showReview: function (review) {
+      console.log(review);
+      this.currentReview = review;
+      document.querySelector("#review-body").showModal();
+    },
+    updateReview: function () {
+      this.editReviewParams.review = this.user.id;
+      // do I need another parameter to make sure that the correct user is editing their review?
+      console.log(this.editReviewParams);
+      axios
+        .patch("/reviews", this.editReviewParams)
+        .then((response) => {
+          console.log(response.data, "the button works?");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          console.log(this.errors);
+        });
+    },
   },
 };
 </script>
@@ -39,7 +62,6 @@ export default {
     <img v-bind:src="user.image_url" alt="" />
     <h2>{{ user.education }}</h2>
     <h3>{{ user.bio }}</h3>
-    <!-- <p>{{ user.earned_reviews }}</p> -->
     <form v-on:submit.prevent="createReview()">
       <h1>Create Review</h1>
       <ul>
@@ -54,6 +76,15 @@ export default {
     <div v-for="review in user.earned_reviews" v-bind:key="review.id">
       <p>{{ review.body }}</p>
       <p>{{ review.rating }}</p>
+      <button v-on:click="showReview(review)">Edit Review</button>
+      <!-- <h1>here's the modal</h1>
+      <SavedModal />
     </div>
+    <SavedModal v-show="showModal" />
+    <div class="save-btn">
+      <button @click="showModal = true">Save</button> -->
+    </div>
+
+    <!-- <SavedModal v-show="showModal" @close-modal="showModal = false" /> -->
   </div>
 </template>
